404325f826cc035212bea354c05403dc02b11ac2
Alessandro De Maria <ademaria@google.com>
2018-05-30 23:14:24 +0000

    Updated

--- kapitan-intro/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-apt-get -qq update && apt-get -qq install -y python3-dev python3-pip python3-yaml libffi-dev libssl-dev && pip3 install --upgrade git+https://github.com/deepmind/kapitan.git --process-dependency-links
