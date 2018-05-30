027b15102a2fac23c3d9f4051b9b3ba9ddb9939b
Alessandro De Maria <ademaria@google.com>
2018-05-30 23:02:29 +0000

    Updated

--- kapitan-intro/install.sh
+++ kapitan-intro/install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo -n Installing Kapitan requirements..  
-apt-get -qq update && apt-get -qq install -y python-dev python3-pip python3-yaml libffi-dev libssl-dev && echo Done
-echo Installing Kapitan...
-pip3 install --upgrade git+https://github.com/deepmind/kapitan.git --process-dependency-links
+apt-get -qq update && apt-get -qq install -y python3-dev python3-pip python3-yaml libffi-dev libssl-dev && pip3 install --upgrade git+https://github.com/deepmind/kapitan.git --process-dependency-links
